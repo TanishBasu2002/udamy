3b4257e322a82e2ed348ecc1da80c91ceda45c8e
Tanish2002Basu <>
2023-02-09 15:42:39 +0530

    dg

---
+++ dalle-clone/client/src/utils/index.js
@@ -0,0 +1,9 @@
+import { surpriseMePrompts } from "..";
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPromt = surpriseMePrompts[randomIndex];
+    if (randomPromt === prompt) {
+        return getRandomPrompt(prompt);
+    }
+    return randomPromt;
+}
